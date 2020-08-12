f752828e2926a2b2f8659f3d004e139969666b64
oleg <csharpdeveloper@list.ru>
2020-08-12 11:43:21 +0600

    compleate

--- index.js
+++ index.js
@@ -6,19 +6,17 @@ const sequelize = require('./database')
 const PORT = process.env.PORT || 3000
 
 app.use(express.static(path.join(__dirname,'public')))
+app.use(express.json())
 app.use('/api/todo',todoRoutes)
 app.use((req,res,next)=>{
     res.sendFile('/index.html')
 })
 
-
-
 //Запускаем сервер, и сервер БД
 async function start(){
 
       try {
-        await sequelize.authenticate();
-        
+        await sequelize.sync();
         console.log('Connection has been established successfully.');
         app.listen(PORT,()=>{
           console.log(`Server is running on port ${PORT}`)});
